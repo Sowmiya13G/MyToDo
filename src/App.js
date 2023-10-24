0d1ba46fafe4c76f09dde0f43adbe5e97a1e84d7
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2023-10-24 11:45:19 +0530

    Persist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import store from './redux/store/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store/store';
 import ToDoScreen from './screens/ToDoScreen/ToDoScreen';
+
 export default function App() {
   return (
     <Provider store={store}>
-      <ToDoScreen />
+      <PersistGate loading={null} persistor={persistor}>
+        <ToDoScreen />
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,19 @@
 import {createStore, combineReducers} from 'redux';
 import taskReducer from '../reducers/reducer';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['tasks'],
+};
 
 const rootReducer = combineReducers({
-  tasks: taskReducer,
+  tasks: persistReducer(persistConfig, taskReducer),
 });
 
 const store = createStore(rootReducer);
+const persistor = persistStore(store);
 
-export default store;
+export {store, persistor};
